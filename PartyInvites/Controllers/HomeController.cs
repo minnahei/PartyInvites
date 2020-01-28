2208ac71c263b8a40da4ff794f307f647d6648c1
minnahei <minna.s.heinonen@gmail.com>
2020-01-28 19:52:05 +0100

    first commit

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet] //resposible for displaying the initial blank form
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost] //resposible for recieving submitted data and deciding what to do with it
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            // TODO: store response from guest
+            return View();
+        }
+    }
+}
